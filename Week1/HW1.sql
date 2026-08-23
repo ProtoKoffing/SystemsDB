2a3ddf4e64ce2b2fc73543a542dad92c34aadc01
ProtoKoffing <jadynbateman18@gmail.com>
2026-08-23 14:54:26 -0600

    Add comments on PostgreSQL database and table creation
    Added comments explaining database creation and table syntax in PostgreSQL.

--- Week1/HW1.sql
+++ Week1/HW1.sql
@@ -5,3 +5,14 @@ select current_database();
 select version();
 
 select 'Hello, SQL!';
+
+/*
+How to create a database in PostgreSQL.
+  CREATE DATABASE [name];
+How to create a table (basic syntax and examples of data types).
+  create table [name](details);
+Why semicolons are important in SQL.
+  Tells us when the commands end.
+At least two examples of data types you might use in a table.
+  Date and numeric
+*/
