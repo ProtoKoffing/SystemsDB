09613d23e3379234857e22fa2f2aaba3e3d88f9e
ProtoKoffing <jadynbateman18@gmail.com>
2026-08-24 11:05:25 -0600

    Added a ; to a line

--- Week2/lab2.sql
+++ Week2/lab2.sql
@@ -3,7 +3,7 @@ CREATE TABLE Animal_Types(species_id integer, species text, habitat text, diet t
 CREATE TABLE Animals(animal_id integer, name text, species_id integer, age integer);
 --Query 2
 INSERT into Animal_Types(species_id, species, habitat, diet)
-VALUES (1, 'Lion', 'Savanah', 'Carnivore'), (2, 'Giraffe', 'Savanah', 'Herbivore'), (3, 'Penguin', 'Arctic', 'Omnivore')
+VALUES (1, 'Lion', 'Savanah', 'Carnivore'), (2, 'Giraffe', 'Savanah', 'Herbivore'), (3, 'Penguin', 'Arctic', 'Omnivore');
 --Query 3
 INSERT into Animals(animal_id, name, species_id, age)
 VALUES (1, 'Simba The Lion', 1, 12), (2, 'Andrea The Giraffe', 2, 18), (3, 'Pingu The Penguin', 3, 22);
