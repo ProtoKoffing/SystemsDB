74cfb77e9b81007925e0b4b2f72cb1b2b455926e
ProtoKoffing <jadynbateman18@gmail.com>
2026-08-24 11:04:33 -0600

    Create Animal_Types and Animals tables with data
    Created tables for animal types and animals, and inserted initial data. Gave an example of an error within an INSERT command.

---
+++ Week2/lab2.sql
@@ -0,0 +1,14 @@
+--Query 1
+CREATE TABLE Animal_Types(species_id integer, species text, habitat text, diet text);
+CREATE TABLE Animals(animal_id integer, name text, species_id integer, age integer);
+--Query 2
+INSERT into Animal_Types(species_id, species, habitat, diet)
+VALUES (1, 'Lion', 'Savanah', 'Carnivore'), (2, 'Giraffe', 'Savanah', 'Herbivore'), (3, 'Penguin', 'Arctic', 'Omnivore')
+--Query 3
+INSERT into Animals(animal_id, name, species_id, age)
+VALUES (1, 'Simba The Lion', 1, 12), (2, 'Andrea The Giraffe', 2, 18), (3, 'Pingu The Penguin', 3, 22);
+--Query 4
+INSERT into Animals(animal_id name, species_id, age)
+VALUES (1, 'Simba The Lion', 1, 12), (2, 'Andrea The Giraffe', 2, 18), (3, 'Pingu The Penguin', 3, 22);
+--Error Test returned ERROR:  syntax error at or near "name"
+--                    LINE 1: INSERT into Animals(animal_id name, species_id, age)
