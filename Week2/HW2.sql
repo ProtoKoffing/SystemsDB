b2c02a2c87273c64cec47b414c10c10fe0112126
ProtoKoffing <jadynbateman18@gmail.com>
2026-08-30 23:53:56 -0600

    Add comments about PgAdmin issues and DISTINCT keyword
    Added comments regarding issues with PgAdmin and observations on the DISTINCT keyword.

---
+++ Week2/HW2.sql
@@ -0,0 +1,5 @@
+-- I was having issues with PgAdmin and could not get it to boot at home after the first time. The quesitons for the notes was all I could do, I'll give more context tomorrow in class.
+
+--The distinct keyword removing duplicate results and only giving unique information is interesting and seems very useful.
+
+--If i do use distinct in two columns does it find the values for each column separately or combine them?
